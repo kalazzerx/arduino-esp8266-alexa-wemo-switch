4649152df35251ba1c74c98e8502680986798fc8
Chris Anderson <chris@kalazzerx.com>
2017-03-30 09:05:33 -0500

    push bach changes

--- sinric/src/sinric.ino
+++ sinric/src/sinric.ino
@@ -10,8 +10,10 @@ void startHttpServer();
 void turnOnRelay();
 void turnOffRelay();
 
-const char* ssid = "Aruna";
-const char* password = "********";
+const char* ssid = "kawifi";
+const char* password = "kalazzerx";
+//const String dname = "shop l1";
+String device_name = "shop l1";
 
 unsigned int localPort = 1900;      // local port to listen on
 
@@ -21,16 +23,17 @@ IPAddress ipMulti(239, 255, 255, 250);
 unsigned int portMulti = 1900;      // local port to listen on
 
 ESP8266WebServer HTTP(80);
- 
+
 boolean wifiConnected = false;
 
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
 
 String serial;
 String persistent_uuid;
-String device_name;
 
-const int relayPin = D1;
+
+const int relayPin = 0;
+const int statusLed = 2;
 
 boolean cannotConnectToWifi = false;
 
@@ -39,55 +42,55 @@ void setup() {
 
   // Setup Relay
   pinMode(relayPin, OUTPUT);
-  
+
   prepareIds();
-  
+
   // Initialise wifi connection
   wifiConnected = connectWifi();
 
   // only proceed if wifi connection successful
   if(wifiConnected){
     udpConnected = connectUDP();
-    
+
     if (udpConnected){
-      // initialise pins if needed 
+      // initialise pins if needed
       startHttpServer();
     }
-  }  
+  }
 }
 
 void loop() {
 
   HTTP.handleClient();
   delay(1);
-  
-  
+
+
   // if there's data available, read a packet
   // check if the WiFi and UDP connections were successful
   if(wifiConnected){
-    if(udpConnected){    
+    if(udpConnected){
       // if there’s data available, read a packet
       int packetSize = UDP.parsePacket();
-      
+
       if(packetSize) {
         Serial.println("");
         Serial.print("Received packet of size ");
         Serial.println(packetSize);
         Serial.print("From ");
         IPAddress remote = UDP.remoteIP();
-        
+
         for (int i =0; i < 4; i++) {
           Serial.print(remote[i], DEC);
           if (i < 3) {
             Serial.print(".");
           }
         }
-        
+
         Serial.print(", port ");
         Serial.println(UDP.remotePort());
-        
+
         int len = UDP.read(packetBuffer, 255);
-        
+
         if (len > 0) {
             packetBuffer[len] = 0;
         }
@@ -95,7 +98,7 @@ void loop() {
         String request = packetBuffer;
         //Serial.println("Request:");
         //Serial.println(request);
-         
+
         if(request.indexOf('M-SEARCH') > 0) {
             if(request.indexOf("urn:Belkin:device:**") > 0) {
                 Serial.println("Responding to search request ...");
@@ -103,11 +106,11 @@ void loop() {
             }
         }
       }
-        
+
       delay(10);
     }
   } else {
-      // Turn on/off to indicate cannot connect ..      
+      // Turn on/off to indicate cannot connect ..
   }
 }
 
@@ -121,7 +124,7 @@ void prepareIds() {
 
   serial = String(uuid);
   persistent_uuid = "Socket-1_0-" + serial;
-  device_name = "box";
+  //device_name = dname;
 }
 
 void respondToSearch() {
@@ -135,7 +138,7 @@ void respondToSearch() {
     char s[16];
     sprintf(s, "%d.%d.%d.%d", localIP[0], localIP[1], localIP[2], localIP[3]);
 
-    String response = 
+    String response =
          "HTTP/1.1 200 OK\r\n"
          "CACHE-CONTROL: max-age=86400\r\n"
          "DATE: Fri, 15 Apr 2016 04:56:29 GMT\r\n"
@@ -150,7 +153,7 @@ void respondToSearch() {
 
     UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
     UDP.write(response.c_str());
-    UDP.endPacket();                    
+    UDP.endPacket();
 
      Serial.println("Response sent !");
 }
@@ -162,16 +165,16 @@ void startHttpServer() {
     });
 
     HTTP.on("/upnp/control/basicevent1", HTTP_POST, []() {
-      Serial.println("########## Responding to  /upnp/control/basicevent1 ... ##########");      
+      Serial.println("########## Responding to  /upnp/control/basicevent1 ... ##########");
 
       //for (int x=0; x <= HTTP.args(); x++) {
       //  Serial.println(HTTP.arg(x));
       //}
-  
-      String request = HTTP.arg(0);      
+
+      String request = HTTP.arg(0);
       Serial.print("request:");
       Serial.println(request);
- 
+
       if(request.indexOf("<BinaryState>1</BinaryState>") > 0) {
           Serial.println("Got Turn on request");
           turnOnRelay();
@@ -181,7 +184,7 @@ void startHttpServer() {
           Serial.println("Got Turn off request");
           turnOffRelay();
       }
-      
+
       HTTP.send(200, "text/plain", "");
     });
 
@@ -214,17 +217,17 @@ void startHttpServer() {
               "</action>"
             "</scpd>\r\n"
             "\r\n";
-            
+
       HTTP.send(200, "text/plain", eventservice_xml.c_str());
     });
-    
+
     HTTP.on("/setup.xml", HTTP_GET, [](){
       Serial.println(" ########## Responding to setup.xml ... ########\n");
 
       IPAddress localIP = WiFi.localIP();
       char s[16];
       sprintf(s, "%d.%d.%d.%d", localIP[0], localIP[1], localIP[2], localIP[3]);
-    
+
       String setup_xml = "<?xml version=\"1.0\"?>"
             "<root>"
              "<device>"
@@ -244,28 +247,28 @@ void startHttpServer() {
                       "<eventSubURL>/upnp/event/basicevent1</eventSubURL>"
                       "<SCPDURL>/eventservice.xml</SCPDURL>"
                   "</service>"
-              "</serviceList>" 
+              "</serviceList>"
               "</device>"
             "</root>\r\n"
             "\r\n";
-            
+
         HTTP.send(200, "text/xml", setup_xml.c_str());
-        
+
         Serial.print("Sending :");
         Serial.println(setup_xml);
     });
-    
-    HTTP.begin();  
+
+    HTTP.begin();
     Serial.println("HTTP Server started ..");
 }
 
 
-      
+
 // connect to wifi – returns true if successful or false if not
 boolean connectWifi(){
   boolean state = true;
   int i = 0;
-  
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -282,7 +285,7 @@ boolean connectWifi(){
     }
     i++;
   }
-  
+
   if (state){
     Serial.println("");
     Serial.print("Connected to ");
@@ -294,16 +297,16 @@ boolean connectWifi(){
     Serial.println("");
     Serial.println("Connection failed.");
   }
-  
+
   return state;
 }
 
 boolean connectUDP(){
   boolean state = false;
-  
+
   Serial.println("");
   Serial.println("Connecting to UDP");
-  
+
   if(UDP.beginMulticast(WiFi.localIP(), ipMulti, portMulti)) {
     Serial.println("Connection successful");
     state = true;
@@ -311,12 +314,12 @@ boolean connectUDP(){
   else{
     Serial.println("Connection failed");
   }
-  
+
   return state;
 }
 
 void turnOnRelay() {
- digitalWrite(relayPin, HIGH); // turn on relay with voltage HIGH 
+ digitalWrite(relayPin, HIGH); // turn on relay with voltage HIGH
 }
 
 void turnOffRelay() {
